652d95b46b21ad39a029e205c6e29da00a6036e6
Wongwc1998 <wongwc1998@gmail.com>
2023-10-23 18:17:13 -0400

    add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,3 +12,11 @@ export type Patient = {
   gender: "male" | "female" | "other";
   occupation: string;
 };
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}
+
+export type NewPatient = Omit<Patient, 'id'>;
